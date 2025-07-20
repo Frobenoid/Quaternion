d97dfc7fb074e60c8e710e06c8bee0ad065c3938
MiltMont <miltgonmont@gmail.com>
2025-07-20 17:14:35 -0600

    Package update to use `@Observable`.

--- Package.swift
+++ Package.swift
@@ -5,16 +5,14 @@ import PackageDescription
 
 let package = Package(
     name: "Quaternion",
+    platforms: [.macOS(.v14)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Quaternion",
             targets: ["Quaternion"]
         )
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "Quaternion"
         ),
@@ -22,9 +20,5 @@ let package = Package(
             name: "QuaternionTests",
             dependencies: ["Quaternion"]
         ),
-//        .testTarget(
-//            name: "ConnectionTests",
-//            dependencies: ["Quaternion"]
-//        ),
     ]
 )
